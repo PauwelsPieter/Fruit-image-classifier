98d218431d0012505fd8377a1f442a7a021e7458
Dries Augustyns <augustyns.dries@gmail.com>
2021-11-17 16:57:22 +0100

    Push

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+import requests
 
-s = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
-driver.maximize_window()
-driver.get('https://www.driaug.com')
-print(driver.title)
+
+def scrape(category):
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.maximize_window()
+    driver.get(f'https://unsplash.com/s/photos/{category}')
+
+    images = driver.find_elements_by_tag_name('img')
+
+    for i, img in enumerate(images):
+        source = img.get_attribute("src")
+        print(source)
+
+        if "profile" in source:
+            continue
+
+        img_data = requests.get(source).content
+        with open(f'./out/{category}-{i}.jpg', 'wb') as handler:
+            handler.write(img_data)
+
+
+scrape('cars')
