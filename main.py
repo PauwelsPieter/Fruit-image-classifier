5f1ed40803573d53e18469882dff47f4985b289d
Dries Augustyns <augustyns.dries@gmail.com>
2021-11-17 16:41:21 +0100

    Init

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+s = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.maximize_window()
+driver.get('https://www.driaug.com')
+print(driver.title)
